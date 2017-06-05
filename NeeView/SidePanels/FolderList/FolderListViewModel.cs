767880697326c3bcb66853ed11c66a0efab72e7d
Mitsuhiro Ito <nee.laboratory@gmail.com>
2017-06-05 21:59:31 +0900

    フォルダーリストの並び替えが機能しない不具合修正

--- NeeView/SidePanels/FolderList/FolderListViewModel.cs
+++ NeeView/SidePanels/FolderList/FolderListViewModel.cs
@@ -114,11 +114,13 @@ namespace NeeView
         private void Model_CollectionChanged(object sender, EventArgs e)
         {
             UpdateListContent();
+            RaisePropertyChanged(nameof(FolderCollection));
             RaisePropertyChanged(nameof(IsFolderRecursive));
             RaisePropertyChanged(nameof(Place));
         }
 
         //
+        public FolderCollection FolderCollection => _model.FolderCollection;
         public bool IsFolderRecursive => _model.FolderCollection != null ? _model.FolderCollection.FolderParameter.IsFolderRecursive : false;
         public string Place => _model.FolderCollection?.PlaceDispString;
 

31784af93a6b04e93eea6dd8be071b126b67831d
Mitsuhiro Ito <nee.laboratory@gmail.com>
2017-07-06 16:00:51 +0900

    ver 1.26 アイコン変更

--- NeeView/VersionWindow.xaml.cs
+++ NeeView/VersionWindow.xaml.cs
@@ -20,6 +20,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
 using NeeView.ComponentModel;
+using System.Windows.Markup;
 
 namespace NeeView
 {
@@ -213,5 +214,57 @@ namespace NeeView
 
             _isCheching = false;
         }
+    }
+
+
+    // from https://stackoverflow.com/questions/952080/how-do-you-select-the-right-size-icon-from-a-multi-resolution-ico-file-in-wpf
+    /// <summary>
+    /// Simple extension for icon, to let you choose icon with specific size.
+    /// Usage sample:
+    /// Image Stretch="None" Source="{common:Icon /Controls;component/icons/custom.ico, 16}"
+    /// Or:
+    /// Image Source="{common:Icon Source={Binding IconResource}, Size=16}"
+    /// </summary> 
+    public class IconExtension : MarkupExtension
+    {
+        private string _source;
+
+        public string Source
+        {
+            get
+            {
+                return _source;
+            }
+            set
+            {
+                // Have to make full pack URI from short form, so System.Uri recognizes it.
+                _source = "pack://application:,,," + value;
+            }
+        }
+
+        public int Size { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var decoder = BitmapDecoder.Create(new Uri(Source),
+                                               BitmapCreateOptions.DelayCreation,
+                                               BitmapCacheOption.OnDemand);
+
+            var result = decoder.Frames.SingleOrDefault(f => f.Width == Size);
+            if (result == default(BitmapFrame))
+            {
+                result = decoder.Frames.OrderBy(f => f.Width).First();
+            }
+
+            return result;
+        }
+
+        public IconExtension(string source, int size)
+        {
+            Source = source;
+            Size = size;
+        }
+
+        public IconExtension() { }
     }
 }

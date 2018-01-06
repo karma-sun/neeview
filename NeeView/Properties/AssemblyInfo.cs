be3bca2f4b341a6eb04cc10e00fffa3cecbc1ed0
Mitsuhiro Ito <nee.laboratory@gmail.com>
2018-01-06 21:51:26 +0900

    assembly version 1.28

--- NeeView/Properties/AssemblyInfo.cs
+++ NeeView/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NeeLaboratory")]
 [assembly: AssemblyProduct("NeeView")]
-[assembly: AssemblyCopyright("Copyright © 2016-2017 Mitsuhiro Ito (nee)")]
+[assembly: AssemblyCopyright("Copyright © 2016-2018 Mitsuhiro Ito (nee)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.27.0.0")]
-[assembly: AssemblyFileVersion("1.27.0.0")]
+[assembly: AssemblyVersion("1.28.0.0")]
+[assembly: AssemblyFileVersion("1.28.0.0")]

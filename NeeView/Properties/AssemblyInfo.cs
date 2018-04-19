dccf79ea621a062fd09d4abd78ac8e8d164678e7
Mitsuhiro Ito <nee.laboratory@gmail.com>
2018-04-19 16:10:18 +0900

    ver 31.0

--- NeeView/Properties/AssemblyInfo.cs
+++ NeeView/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("30.0.0.0")]
-[assembly: AssemblyFileVersion("30.0.0.0")]
+[assembly: AssemblyVersion("31.0.0.0")]
+[assembly: AssemblyFileVersion("31.0.0.0")]

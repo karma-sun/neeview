7a540089a969b81fdb230a92c71e99e6b65457a0
Mitsuhiro Ito <nee.laboratory@gmail.com>
2019-03-22 23:50:38 +0900

    - マージ削除されたファイルの復活

--- NeeView/MouseInput/MouseInputHelper.cs
+++ NeeView/MouseInput/MouseInputHelper.cs
@@ -11,18 +11,6 @@ namespace NeeView
     /// </summary>
     public static class MouseInputHelper
     {
-        /// <summary>
-        /// Deltaを回数に変換
-        /// </summary>
-        /// <param name="e"></param>
-        /// <returns></returns>
-        internal static int DeltaCount(MouseWheelEventArgs e)
-        {
-            int count = Math.Abs(e.Delta) / 120;
-            if (count < 1) count = 1;
-            return count;
-        }
-
         #region MouseCapture Helper
         // MouseCaptureのラップ。開発用
 

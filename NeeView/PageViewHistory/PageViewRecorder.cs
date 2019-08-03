fda338eaa2b24f2b04d82121d5a59d091b09beb5
squld <sato@kurusugawa.jp>
2019-08-03 16:58:57 +0900

    ページ表示期間とブック表示期間を書き込むようにした。 ファイル書き込みの排他制御を行った。

--- NeeView/PageViewHistory/PageViewRecorder.cs
+++ NeeView/PageViewHistory/PageViewRecorder.cs
@@ -9,6 +9,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Text;
 
 namespace NeeView
 {
@@ -23,7 +24,13 @@ namespace NeeView
 
         private bool _isRecordPageView;
         private string _pageViewRecordPath;
-        private TextWriter _writer;
+        private FileStream _file;
+        private StringBuilder _writeBuffer;
+        private DateTime _viewedPagesDateTime;
+        private List<Page> _viewedPages;
+        private DateTime _viewedBookDateTime;
+        private string _viewedBookAddress;
+        private string _viewedBookName;
         private object _lock = new object();
 
         #endregion Fields
@@ -60,33 +67,127 @@ namespace NeeView
 
         #endregion Properties
 
-        #region IDisposable Support
-        public void Dispose()
+        private void WritePageViewedRecord(DateTime now)
         {
             lock (_lock)
             {
-                if (_writer != null)
+                if (_file == null)
                 {
-                    _writer.Write(DateTime.Now.ToString("O"));
-                    _writer.Write('\t');
-                    _writer.Write("[Exit] ");
-                    _writer.Write(Config.Current.ApplicationName);
-                    _writer.Write(' ');
-                    _writer.Write(Config.Current.DispVersion);
-                    _writer.WriteLine();
+                    return;
+                }
+
+                foreach (var page in _viewedPages)
+                {
+                    _writeBuffer.Clear();
+                    _writeBuffer.Append(_viewedPagesDateTime.ToString("O"));
+                    _writeBuffer.Append('\t');
+                    switch (page.PageType)
+                    {
+                        case PageType.Folder:
+                            _writeBuffer.Append("Folder");
+                            break;
+                        case PageType.File:
+                            _writeBuffer.Append("File");
+                            break;
+                        default:
+                            _writeBuffer.Append("Unknown");
+                            break;
+                    }
+                    _writeBuffer.Append('\t');
+                    _writeBuffer.Append((now - _viewedPagesDateTime).TotalSeconds.ToString("#0.0000000"));
+                    _writeBuffer.Append('\t');
+                    _writeBuffer.Append(page.BookPrefix);
+                    _writeBuffer.Append('\t');
+                    _writeBuffer.Append(page.EntryFullName);
+                    _writeBuffer.AppendLine();
+                }
+
+                try
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(_writeBuffer.ToString());
+                    _file.Seek(0L, SeekOrigin.End);
+                    _file.Write(bytes, 0, bytes.Length);
+                    _file.Flush();
+                }
+                catch (IOException err)
+                {
+                    Debug.WriteLine("[Error] {0}", err.Message);
+                    ToastService.Current.Show(new Toast(Resources.DialogPageViewRecordWriteError, null, ToastIcon.Error));
                 }
             }
-            CloseWriter();
         }
 
-        private void CloseWriter()
+        private void WriteBookViewedRecord(DateTime now)
+        {
+            lock (_lock)
+            {
+                if (_file == null)
+                {
+                    return;
+                }
+
+                if (_viewedBookAddress == null)
+                {
+                    return;
+                }
+
+                _writeBuffer.Clear();
+                _writeBuffer.Append(_viewedBookDateTime.ToString("O"));
+                _writeBuffer.Append('\t');
+                _writeBuffer.Append("Book");
+                _writeBuffer.Append('\t');
+                _writeBuffer.Append((now - _viewedBookDateTime).TotalSeconds.ToString("#0.0000000"));
+                _writeBuffer.Append('\t');
+                _writeBuffer.Append(_viewedBookAddress);
+                _writeBuffer.Append('\t');
+                _writeBuffer.Append(_viewedBookName);
+                _writeBuffer.AppendLine();
+
+                try
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(_writeBuffer.ToString());
+                    _file.Seek(0L, SeekOrigin.End);
+                    _file.Write(bytes, 0, bytes.Length);
+                    _file.Flush();
+                }
+                catch (IOException err)
+                {
+                    Debug.WriteLine("[Error] {0}", err.Message);
+                    ToastService.Current.Show(new Toast(Resources.DialogPageViewRecordWriteError, null, ToastIcon.Error));
+                }
+            }
+        }
+
+        private void OpenFile()
+        {
+            lock (_lock)
+            {
+                try
+                {
+                    _file = File.Open(PageViewRecordPath, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+                    _writeBuffer = new StringBuilder(1024);
+                }
+                catch (IOException err)
+                {
+                    Debug.WriteLine("[Error] {0}", err.Message);
+                    ToastService.Current.Show(new Toast(Resources.DialogPageViewRecordWriteError, null, ToastIcon.Error));
+                }
+                _viewedBookAddress = null;
+                _viewedPages = new List<Page>();
+                BookHub.Current.BookChanged += OnViewBookChanged;
+                BookHub.Current.ViewContentsChanged += OnViewContentsChanged;
+            }
+        }
+
+        private void CloseFile()
         {
             lock (_lock)
             {
                 BookHub.Current.ViewContentsChanged -= OnViewContentsChanged;
+                BookHub.Current.BookChanged -= OnViewBookChanged;
                 try
                 {
-                    _writer?.Close();
+                    _file?.Close();
                 }
                 catch (Exception err)
                 {
@@ -94,47 +195,62 @@ namespace NeeView
                 }
                 finally
                 {
-                    _writer = null;
+                    _writeBuffer = null;
+                    _file = null;
                 }
             }
         }
+
+        #region IDisposable Support
+        public void Dispose()
+        {
+            var now = DateTime.Now;
+            WritePageViewedRecord(now);
+            WriteBookViewedRecord(now);
+            CloseFile();
+        }
+
         #endregion
 
         #region Callback Methods
 
-        private void OnViewContentsChanged(object sender, ViewContentSourceCollectionChangedEventArgs e)
+        private void OnViewBookChanged(object sender, BookChangedEventArgs e)
         {
             var now = DateTime.Now;
+            var book = BookHub.Current.Book;
+            if (book == null)
+            {
+                return;
+            }
 
             AppDispatcher.Invoke(() =>
             {
-                var viewPages = e?.ViewPageCollection?.Collection.Where(x => x != null).Select(x => x.Page).ToList() ?? new List<Page>();
+                WriteBookViewedRecord(now);
 
-                lock (_lock)
+                _viewedBookDateTime = now;
+                _viewedBookAddress = book.Address;
+                if (book?.NotFoundStartPage != null && book.Pages.Count > 0)
                 {
-                    if (_writer == null)
-                    {
-                        return;
-                    }
-
-                    try
-                    {
-                        foreach (var page in viewPages)
-                        {
-                            _writer.Write(now.ToString("O"));
-                            _writer.Write('\t');
-                            _writer.Write(page.BookPrefix);
-                            _writer.Write('\t');
-                            _writer.Write(page.EntryFullName);
-                            _writer.WriteLine();
-                        }
-                    }
-                    catch (IOException err)
-                    {
-                        Debug.WriteLine("[Error] {0}", err.Message);
-                        ToastService.Current.Show(new Toast(Resources.DialogPageViewRecordWriteError, null, ToastIcon.Error));
-                    }
+                    _viewedBookName = string.Format(Resources.NotifyCannotOpen, LoosePath.GetFileName(book.NotFoundStartPage));
                 }
+                else
+                {
+                    _viewedBookName = LoosePath.GetFileName(book.Address);
+                }
+
+            });
+        }
+
+        private void OnViewContentsChanged(object sender, ViewContentSourceCollectionChangedEventArgs e)
+        {
+            var now = DateTime.Now;
+            var viewedPages = e?.ViewPageCollection?.Collection.Where(x => x != null).Select(x => x.Page).ToList() ?? new List<Page>();
+
+            AppDispatcher.Invoke(() =>
+            {
+                WritePageViewedRecord(now);
+                _viewedPagesDateTime = now;
+                _viewedPages = viewedPages;
             });
         }
 
@@ -142,30 +258,19 @@ namespace NeeView
         {
             AppDispatcher.Invoke(() =>
             {
-                lock (_lock)
+                CloseFile();
+
+                if (!IsRecordPageView)
                 {
-                    CloseWriter();
-                    if (!IsRecordPageView)
-                    {
-                        return;
-                    }
-
-                    if (String.IsNullOrEmpty(PageViewRecordPath))
-                    {
-                        return;
-                    }
-
-                    try
-                    {
-                        _writer = new StreamWriter(PageViewRecordPath, true);
-                    }
-                    catch (IOException err)
-                    {
-                        Debug.WriteLine("[Error] {0}", err.Message);
-                        ToastService.Current.Show(new Toast(Resources.DialogPageViewRecordWriteError, null, ToastIcon.Error));
-                    }
-                    BookHub.Current.ViewContentsChanged += OnViewContentsChanged;
+                    return;
                 }
+
+                if (String.IsNullOrEmpty(PageViewRecordPath))
+                {
+                    return;
+                }
+
+                OpenFile();
             });
         }
 

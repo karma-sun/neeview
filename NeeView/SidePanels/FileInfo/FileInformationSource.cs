ac639bcc3d6ea5a4e82cccc50e38589fd2a893a3
Ito Mitsuhiro <nee.laboratory@gmail.com>
2021-02-17 11:58:47 +0900

    情報パネルのサムネイルがが画像でない場合に拡張子を表示するようにした

--- NeeView/SidePanels/FileInfo/FileInformationSource.cs
+++ NeeView/SidePanels/FileInfo/FileInformationSource.cs
@@ -1,5 +1,8 @@
 ﻿using NeeLaboratory.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 
 namespace NeeView
 {
@@ -21,13 +24,9 @@ namespace NeeView
 
         public BitmapExif Exif => PictureInfo?.Exif;
 
-        public double ThumbnailMaxSize => 96.0;
+        public double IconMaxSize => 96.0;
 
-        public double ThumbnailWidth => BitmapContent != null ? BitmapContent.Size.Width * GetAspectoRatio() : 0.0;
-
-        public double ThumbnailHeight => BitmapContent != null ? BitmapContent.Size.Height * GetAspectoRatio() : 0.0;
-
-        public ImageSource ImageSource => BitmapContent?.ImageSource;
+        public FrameworkElement Icon => CreateIcon();
 
         public string FullPath => ViewContent?.Page?.Entry?.Link ?? ViewContent?.FullPath;
 
@@ -52,16 +51,117 @@ namespace NeeView
         public string Decoder => ((BitmapContent is AnimatedContent animatedContent && animatedContent.IsAnimated) || BitmapContent is MediaContent) ? "MediaPlayer" : PictureInfo?.Decoder;
 
 
-        private double GetAspectoRatio()
-        {
-            if (BitmapContent is null) return 1.0;
-            var length = BitmapContent.Size.Width > BitmapContent.Size.Height ? BitmapContent.Size.Width : BitmapContent.Size.Height;
-            return ThumbnailMaxSize / length;
-        }
 
         public void Update()
         {
             RaisePropertyChanged(null);
+        }
+
+
+        public FrameworkElement CreateIcon()
+        {
+            if (BitmapContent?.ImageSource != null)
+            {
+                return CreateBitmapContentIcon(BitmapContent);
+            }
+            else if (Page?.Entry != null)
+            {
+                var entry = Page.Entry;
+
+                if (entry.IsDirectory)
+                {
+                    if (entry.IsFileSystem)
+                    {
+                        return CreateSymbolFolderIcon();
+                    }
+                    else
+                    {
+                        return CreateSymbolIcon("/Archive");
+                    }
+                }
+                else
+                {
+                    return CreateSymbolIcon(LoosePath.GetExtension(entry.EntryName).ToUpper());
+                }
+            }
+
+            return null;
+        }
+
+        private FrameworkElement CreateBitmapContentIcon(BitmapContent bitmapContent)
+        {
+            if (bitmapContent?.ImageSource is null) return null;
+
+            var length = bitmapContent.Size.Width > bitmapContent.Size.Height ? bitmapContent.Size.Width : bitmapContent.Size.Height;
+            var retio = IconMaxSize / length;
+
+            var image = new Image()
+            {
+                Source = bitmapContent.ImageSource,
+                Width = bitmapContent.Size.Width * retio,
+                Height = bitmapContent.Size.Height * retio,
+                Stretch = Stretch.Fill,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Bottom,
+                UseLayoutRounding = true,
+                Effect = new DropShadowEffect()
+                {
+                    ShadowDepth = 2.0,
+                    Opacity = 0.5
+                },
+            };
+            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.HighQuality);
+
+            return image;
+        }
+
+        private FrameworkElement CreateSymbolFolderIcon()
+        {
+            var image = new Image()
+            {
+                Source = FileIconCollection.Current.CreateDefaultFolderIcon().GetBitmapSource(48.0),
+                Width = 48.0,
+                Height = 48.0,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                UseLayoutRounding = true,
+            };
+            return CreateSymbolIcon(image);
+        }
+
+        private FrameworkElement CreateSymbolIcon(string text)
+        {
+            var border = new Border()
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                MaxWidth = IconMaxSize,
+                Child = new TextBlock()
+                {
+                    Text = text,
+                    FontSize = 20.0,
+                    FontWeight = FontWeights.Bold,
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Margin = new Thickness(5),
+                },
+            };
+            return CreateSymbolIcon(border);
+        }
+
+        private FrameworkElement CreateSymbolIcon(UIElement content)
+        {
+            var border = new Border()
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Width = IconMaxSize,
+                Height = IconMaxSize,
+                Background = new SolidColorBrush(Color.FromArgb(0x20, 0x80, 0x80, 0x80)),
+                Child = content,
+            };
+
+            return border;
         }
 
     }

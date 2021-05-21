a050cb8bb79a89b397b94904c779a700f398a34f
Ito Mitsuhiro <nee.laboratory@gmail.com>
2021-05-21 20:51:06 +0900

    - フォント設定修正

--- NeeView/System/FontParameters.cs
+++ NeeView/System/FontParameters.cs
@@ -55,7 +55,10 @@ namespace NeeView
 
                     var arrowFontFamily = new FontFamily();
                     arrowFontFamily.FamilyMaps.Add(new FontFamilyMap() { Unicode = "2190-2193", Target = "Calibri" }); // 矢印フォントだけ變更
-                    arrowFontFamily.FamilyMaps.Add(new FontFamilyMap() { Unicode = "0000-10ffff", Target = _defaultFontName ?? "" });
+                    if (!string.IsNullOrWhiteSpace(_defaultFontName))
+                    {
+                        arrowFontFamily.FamilyMaps.Add(new FontFamilyMap() { Unicode = "0000-10ffff", Target = _defaultFontName });
+                    }
                     App.Current.Resources["ArrowFontFamily"] = arrowFontFamily;
                 }
             }
